9cdd2ca6ffe0755a8855f7c520a550b94cffe25d
dvpandress <andresmedel64@gmail.com>
2025-07-04 19:00:33 -0400

    Update Box.java

--- src/main/java/net/moonly/Box.java
+++ src/main/java/net/moonly/Box.java
@@ -59,7 +59,7 @@ public final class Box extends JavaPlugin implements Listener {
     private int autoRespawnDelayTicks; // Para el nuevo modo de AutoRespawn con delay
 
     @Override
-    public void OneNableasd() {
+    public void onEnable() {
         instance = this;
 
         saveDefaultConfig();
